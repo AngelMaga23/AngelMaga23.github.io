34076af73ec67574fac38dd51140e3f489a31f94
AngelMaga23 <66535093+AngelMaga23@users.noreply.github.com>
2023-05-04 15:07:53 -0600

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.container-projects', { delay: 500 });
+ScrollReveal().reveal('.footer-links', { delay: 500 });
+// ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
